d36da9d4a0e9c4fc3447d0adc8a98ce9b1cdde8a
shk0625 <tjfgml1238@gmail.com>
2023-04-17 20:38:31 +0900

    import upload

--- src/main/java/aws/example/springs3/service/S3Upload.java
+++ src/main/java/aws/example/springs3/service/S3Upload.java
@@ -6,6 +6,7 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.UUID;
