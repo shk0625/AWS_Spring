a9fabef1276a40fbf068c75f5941fbede1e53229
shk0625 <tjfgml1238@gmail.com>
2023-04-17 20:22:52 +0900

    service

---
+++ src/main/java/aws/example/springs3/controller/FileUploadController.java
@@ -0,0 +1,12 @@
+package aws.example.springs3.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class FileUploadController {
+
+
+
+}
