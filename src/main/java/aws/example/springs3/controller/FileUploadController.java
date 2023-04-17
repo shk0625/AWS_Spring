558101c2970cbe2c05031a0001af26f292c1cebf
shk0625 <tjfgml1238@gmail.com>
2023-04-17 20:26:14 +0900

    controller

--- src/main/java/aws/example/springs3/controller/FileUploadController.java
+++ src/main/java/aws/example/springs3/controller/FileUploadController.java
@@ -1,12 +1,26 @@
 package aws.example.springs3.controller;
 
+import aws.example.springs3.service.S3Upload;
 import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @RestController
 public class FileUploadController {
 
+    private final S3Upload s3Upload;
 
+    @PostMapping("/upload")
+    public <ApiResponse> ApiResponse<String> uploadFile(@RequestParam("images")MultipartFile multipartFile)
+        throws IOException {
+        return ApiResponse.success(HttpStatus.CREATED, s3Upload.upload(multipartFile.getInputStream(),
+                multipartFile.getOriginalFilename(), fileSize));
+    }
 
 }
