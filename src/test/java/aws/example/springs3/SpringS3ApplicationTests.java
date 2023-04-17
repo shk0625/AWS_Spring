ac46c6acb0e1b15ada5258b77b047dde6b7afdce
shk0625 <tjfgml1238@gmail.com>
2023-04-17 12:08:42 +0900

    base

---
+++ src/test/java/aws/example/springs3/SpringS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package aws.example.springs3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringS3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
